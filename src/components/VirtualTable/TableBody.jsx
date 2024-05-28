14bee563e6795c27a1178d597a0d5e9b5d3583e6
luhaikun <lhk2014@163.com>
2024-05-28 03:40:34 +0000

    fix: 优化渲染

--- src/components/VirtualTable/TableBody.jsx
+++ src/components/VirtualTable/TableBody.jsx
@@ -1,36 +1,33 @@
-import { useRef, useState, useLayoutEffect } from 'react'
-import TableContext from './TableContext'
+import { useRef, useState, useLayoutEffect } from "react";
+import TableContext from "./TableContext";
 
-const ITEM_HEIGHT = 50; // 每⾏⾼度
 function TableBody(props) {
-  const wrapper = useRef() // 滚动元素
-  const [start, setStart] = useState(0) //开始渲染的位置
-  const totalHeight = props.data.length * ITEM_HEIGHT // 总⾼度
+  const wrapper = useRef(); // 滚动元素
 
   useLayoutEffect(() => {
-    const scrollEl = props.tbody.current?.querySelector('.ant-table-body')
-    if (!scrollEl) return
+    const scrollEl = props.tbody.current?.querySelector(".ant-table-body");
+    if (!scrollEl) return;
     const handleScroll = () => {
       // 计算开始渲染的位置
-      scrollEl && setStart(Math.floor(scrollEl.scrollTop / ITEM_HEIGHT))
-    }
-    scrollEl && scrollEl.addEventListener('scroll', handleScroll)
+      scrollEl && props.setStart(Math.floor(scrollEl.scrollTop / 50));
+    };
+    scrollEl && scrollEl.addEventListener("scroll", handleScroll);
 
     return () => {
-      scrollEl && scrollEl.removeEventListener('scroll', handleScroll)
-    }
-  }, [wrapper.current, props.tbody.current])
+      scrollEl && scrollEl.removeEventListener("scroll", handleScroll);
+    };
+  }, [wrapper.current, props.tbody.current]);
 
-  const translateY = start * ITEM_HEIGHT // 偏移量
+  const translateY = props.start * 50; // 偏移量
   return (
-    <div ref={wrapper} style={{height: totalHeight, width: 1500}}>
-      <div style={{transform: `translate3d(0, ${translateY}px, 0)`}}>
-        <TableContext.Provider value={{data: props.data, start}}>
+    <div ref={wrapper} style={{ height: props.totalHeight, width: 1500 }}>
+      <div style={{ transform: `translate3d(0, ${translateY}px, 0)` }}>
+        <TableContext.Provider value={{ data: props.data, start: props.start }}>
           <tbody {...props} />
         </TableContext.Provider>
       </div>
     </div>
-  )
+  );
 }
 
 export default TableBody;
--- src/components/VirtualTable/index.js
+++ src/components/VirtualTable/index.js
@@ -1,79 +1,81 @@
-import { useRef, useMemo } from 'react'
-import { Table } from 'antd'
-import TableBody from './TableBody'
-import TableRow from './TableRow'
+import { useRef, useMemo, useState } from "react";
+import { Table } from "antd";
+import TableBody from "./TableBody";
 
+const ITEM_HEIGHT = 50; // 每⾏⾼度
+const SCROLL_HEIGHT = 500; // 滚动区域⾼度
+const showNum = Math.ceil(SCROLL_HEIGHT / ITEM_HEIGHT); // 显⽰的数量
 const columns = [
   {
-    title: 'Full Name',
+    title: "Full Name",
     width: 100,
-    dataIndex: 'name',
-    key: 'name'
+    dataIndex: "name",
+    key: "name",
   },
   {
-    title: 'Age',
+    title: "Age",
     width: 100,
-    dataIndex: 'age',
-    key: 'age'
+    dataIndex: "age",
+    key: "age",
   },
   {
-    title: 'Column 1',
-    dataIndex: 'address',
-    key: '1',
+    title: "Column 1",
+    dataIndex: "address",
+    key: "1",
     width: 150,
   },
   {
-    title: 'Column 2',
-    dataIndex: 'address',
-    key: '2',
+    title: "Column 2",
+    dataIndex: "address",
+    key: "2",
     width: 150,
   },
   {
-    title: 'Column 3',
-    dataIndex: 'address',
-    key: '3',
+    title: "Column 3",
+    dataIndex: "address",
+    key: "3",
     width: 150,
   },
   {
-    title: 'Column 4',
-    dataIndex: 'address',
-    key: '4',
+    title: "Column 4",
+    dataIndex: "address",
+    key: "4",
     width: 150,
   },
   {
-    title: 'Column 5',
-    dataIndex: 'address',
-    key: '5',
+    title: "Column 5",
+    dataIndex: "address",
+    key: "5",
     width: 150,
   },
   {
-    title: 'Column 6',
-    dataIndex: 'address',
-    key: '6',
+    title: "Column 6",
+    dataIndex: "address",
+    key: "6",
     width: 150,
   },
   {
-    title: 'Column 7',
-    dataIndex: 'address',
-    key: '7',
+    title: "Column 7",
+    dataIndex: "address",
+    key: "7",
     width: 150,
   },
   {
-    title: 'Column 8',
-    dataIndex: 'address',
-    key: '8',
+    title: "Column 8",
+    dataIndex: "address",
+    key: "8",
   },
   {
-    title: 'Action',
-    key: 'operation',
-    fixed: 'right',
+    title: "Action",
+    key: "operation",
+    fixed: "right",
     width: 100,
     render: () => <a>action</a>,
   },
 ];
 
 function VirtualTable() {
-  const tableRef = useRef()
+  const tableRef = useRef();
   const data = useMemo(() => {
     const data = [];
     for (let i = 0; i < 10000; i++) {
@@ -84,14 +86,18 @@ function VirtualTable() {
         address: `London Park no. ${i}`,
       });
     }
-    return data
+    return data;
   }, []);
+  const [start, setStart] = useState(0); //开始渲染的位置
+  const totalHeight = data.length * ITEM_HEIGHT; // 总⾼度
+
+  const dataList = data.slice(start, start + showNum);
   return (
     <Table
-      rowKey='key'
+      rowKey="key"
       ref={tableRef}
       columns={columns}
-      dataSource={data}
+      dataSource={dataList}
       scroll={{
         x: 1500,
         y: 500,
@@ -99,9 +105,17 @@ function VirtualTable() {
       components={{
         body: {
           // 重写body属性
-          wrapper: (props) => <TableBody {...props} tbody={tableRef} />,
-          row: (props) => <TableRow {...props} />
-        }
+          wrapper: (props) => (
+            <TableBody
+              data={data}
+              {...props}
+              start={start}
+              setStart={setStart}
+              totalHeight={totalHeight}
+              tbody={tableRef}
+            />
+          ),
+        },
       }}
       pagination={false}
     />
